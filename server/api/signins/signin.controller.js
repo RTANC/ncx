1cbcb59366d588de5d057f0891fc1af1fb5ac4a1
RTANC <wongsathon_n@rtanc.ac.th>
2019-03-26 21:55:19 +0700

    fix signin not user

--- server/api/signins/signin.controller.js
+++ server/api/signins/signin.controller.js
@@ -7,15 +7,19 @@ exports.userSignin = async (req, res, next) => {
         const ticket = await axios.get('https://oauth2.googleapis.com/tokeninfo?id_token=' + req.headers.authorization.split(" ")[1])
         const payload = ticket.data
         const user = await User.findOne({ where: { googleId: payload.sub }})
-        await Log.create({ userId: user.userId })
-        const token = await jwt.sign({ userId: user.userId, role: user.role, deptId: user.deptId, stdId: user.stdId }, process.env.JWT_KEY, { expiresIn: '1d' })
-        res.status(200).send({
-            userId: user.userId,
-            role: user.role,
-            deptId: user.deptId,
-            stdId: user.stdId,
-            token: token
-        })
+        if (!user) {
+            res.status(200).send({ userId: null })
+        } else {
+            await Log.create({ userId: user.userId })
+            const token = await jwt.sign({ userId: user.userId, role: user.role, deptId: user.deptId, stdId: user.stdId }, process.env.JWT_KEY, { expiresIn: '1d' })
+            res.status(200).send({
+                userId: user.userId,
+                role: user.role,
+                deptId: user.deptId,
+                stdId: user.stdId,
+                token: token
+            })
+        }
     } catch (error) {
         next(error)
     }
